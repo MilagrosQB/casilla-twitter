5e359a59ec743c4e0426bc0900443c268d1d05b2
MilagrosQB <qbmilagros@gmail.com>
2016-09-19 15:56:20 -0500

    iniciando proyecto

---
+++ js/app.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function() {
+	var button = document.getElementById("tweet");
+	button.addEventListener("click", function() {
+			var mensaje = document.getElementById("mensaje").value;
+			reTwitter(mensaje);
+		});
+
+	function reTwitter(mensaje) {
+		var divjr = document.createElement("div");
+		var divdaddy = document.getElementById("divdaddy");
+	
+		divjr.innerText = mensaje;
+
+		if(!divdaddy.childNodes[0]){
+			divdaddy.appendChild(divjr);
+		}else{
+			divdaddy.insertBefore(divjr, divdaddy.childNodes[0]);
+		}
+	}
+});
