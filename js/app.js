6b5a1399d6ee8d414110e3655a4deebe6f8c7a94
MilagrosQB <qbmilagros@gmail.com>
2016-09-20 15:32:43 -0500

    tercer tag final

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", function() {
 	var button = document.getElementById("tweet");
-	button.disabled=true;
+	button.disabled = true;
 	button.addEventListener("click", function() {
 		event.preventDefault();
 		var mensaje = document.getElementById("mensaje").value;
 		reTwitter(mensaje);
 		document.getElementById("mensaje").value = "";
-		});
+		contador.textContent = "140";
+	});
 
 	function reTwitter(mensaje) {
 		var divjr = document.createElement("div");
@@ -21,25 +22,38 @@ window.addEventListener("load", function() {
 		}
 	}
 
-window.addEventListener("keydown",function(){
-	button.disabled = false;
-	contador(mensaje);
+	mensaje.addEventListener("keydown",function(){
+		button.disabled = false;
+		contador(mensaje);
+		});
+
+		function contador(mensaje){
+			var limite = 140;
+			var longitud = document.getElementById("mensaje").value.length;
+			document.getElementById("contador").innerHTML= limite-longitud;
+			if((limite-longitud) <= 130){
+				document.getElementById("contador").style.color = "#0000FF";
+			}
+			if((limite-longitud) <= 120){
+				document.getElementById("contador").style.color = "#A901DB";
+			}
+			if(longitud >= limite) {
+				button.disabled = true;
+				document.getElementById("contador").style.color = "#FF0000";
+			}
+		}
 	});
 
-	function contador(mensaje){
-		var limite = 140;
-		var longitud = document.getElementById("mensaje").value.length;
-		document.getElementById("contador").innerHTML= limite-longitud;
-		if((limite-longitud) <= 130){
-			document.getElementById("contador").style.color = "#0000FF";
-		}
-		if((limite-longitud) <= 120){
-			document.getElementById("contador").style.color = "#A901DB";
-		}
+	mensaje.addEventListener("keydown", autosize);
+		button.disabled = false;             
+	function autosize(){
+	  var size = this;
+	  setTimeout(function(){
+	    size.style.cssText = "height:auto; padding:10";
+	    size.style.cssText = "height:" + size.scrollHeight +"px";
+	    },0);
+		
 		if(longitud >= limite) {
-			button.disabled = true;
-			document.getElementById("contador").style.color = "#FF0000";
+		button.disabled = true;
 		}
 	}
-});
-
